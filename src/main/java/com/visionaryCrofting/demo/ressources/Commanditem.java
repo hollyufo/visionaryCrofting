4c88e8a88b78983f0949f9a62841f3b8fef237a5
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-11-28 10:10:12 +0100

    Create Commanditem.java

---
+++ src/main/java/com/visionaryCrofting/demo/ressources/Commanditem.java
@@ -0,0 +1,14 @@
+package com.visionaryCrofting.demo.ressources;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+
+@RestController
+@RequestMapping(path = "Command")
+public class Commanditem {
+    
+}
