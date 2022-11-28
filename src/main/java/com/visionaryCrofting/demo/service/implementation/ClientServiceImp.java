214326004c7c3d0b948ace55e9530ae6f0834b88
Ahmed Salim <93927978+AHMEDSALIM01@users.noreply.github.com>
2022-11-28 19:25:20 +0100

    Création d'un Service pour client

--- src/main/java/com/visionaryCrofting/demo/service/implementation/ClientServiceImp.java
+++ src/main/java/com/visionaryCrofting/demo/service/implementation/ClientServiceImp.java
@@ -1,14 +1,15 @@
-package com.visionaryCrofting.demo.service;
+package com.visionaryCrofting.demo.service.implementation;
 
 import com.visionaryCrofting.demo.entity.Client;
 import com.visionaryCrofting.demo.repositories.ClientRepository;
+import com.visionaryCrofting.demo.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class ClientServiceImp implements ClientService{
+public class ClientServiceImp implements ClientService {
     @Autowired
     ClientRepository clientRepository;
     @Override
