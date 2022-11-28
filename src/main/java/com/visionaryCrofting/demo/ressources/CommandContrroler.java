f8a8cf368bb778f9a2413bf217b82b3ae6e2ea9e
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-11-28 11:00:43 +0100

    Update CommandContrroler.java

--- src/main/java/com/visionaryCrofting/demo/ressources/CommandContrroler.java
+++ src/main/java/com/visionaryCrofting/demo/ressources/CommandContrroler.java
@@ -1,11 +1,14 @@
 package com.visionaryCrofting.demo.ressources;
 
 
+import com.visionaryCrofting.demo.entity.CommandeItem;
 import com.visionaryCrofting.demo.service.CommandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Command")
@@ -16,7 +19,7 @@ public class CommandContrroler {
         this.commandService = commandService;
     }
     @GetMapping("/all")
-    public String getAllCommandeItem(){
-        return commandService.getAllCommandeItem().toString();
+    public List<CommandeItem> getAllCommandeItem(){
+        return commandService.getAllCommandeItem();
     }
 }
