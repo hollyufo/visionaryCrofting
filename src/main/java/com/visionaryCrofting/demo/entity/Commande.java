f9887596b850f99d60fa867bdeb9bcfd3391fb03
Ahmed Salim <93927978+AHMEDSALIM01@users.noreply.github.com>
2022-11-29 15:52:46 +0100

    modification sur les entités

--- src/main/java/com/visionaryCrofting/demo/entity/Commande.java
+++ src/main/java/com/visionaryCrofting/demo/entity/Commande.java
@@ -1,6 +1,10 @@
 package com.visionaryCrofting.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -9,7 +13,7 @@ import java.util.List;
 @Table(name = "Commande",uniqueConstraints = {
         @UniqueConstraint(columnNames = "ref")
 })
-public class Commande {
+public class Commande implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "commande_id")
@@ -25,7 +29,8 @@ public class Commande {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private Client client;
-   @OneToMany(mappedBy = "commande",cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "commande",cascade = CascadeType.ALL)
+    @JsonManagedReference
     private List<CommandeItem> commandeItems;
     public Commande(){}
     public Commande(String ref, LocalDate date, Double prixTotal, Client client) {
