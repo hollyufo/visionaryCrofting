c210742a84affae6a2018562cd20cf37aa3d731a
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-11-30 14:53:45 +0100

    Update FornisseurService.java

--- src/main/java/com/visionaryCrofting/demo/service/FornisseurService.java
+++ src/main/java/com/visionaryCrofting/demo/service/FornisseurService.java
@@ -1,4 +1,16 @@
 package com.visionaryCrofting.demo.service;
 
+import com.visionaryCrofting.demo.entity.Fournisseur;
+
+import java.util.List;
+
 public interface FornisseurService {
+
+    List<Fournisseur> getAllFournisseur();
+    Fournisseur getFournisseurById(Long id);
+    Fournisseur getFournisseurByEmail(String email);
+    Fournisseur addFournisseur(Fournisseur fournisseur);
+    String deleteFournisseur(Long id);
+    Fournisseur updateFournisseur(Long id, Fournisseur fournisseur);
+
 }
